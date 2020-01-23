7b45713936e789409f3bc16e174d657f6ebdea52
asiahub-10 <asiarahman104@gmail.com>
2020-01-23 16:45:56 +0600

    project uploaded

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// auto rotation slider
+
+$(function() {
+    $('#third').carouseller({
+        scrollSpeed: 800,
+        autoScrollDelay: 1600,
+        easing: 'linear'
+    });
+});
+
+
+$(function() {
+    $('#comment').carouseller({
+        scrollSpeed: 800,
+        autoScrollDelay: 3000,
+        easing: 'linear'
+    });
+});
+
+$(function() {
+    $('#brand').carouseller({
+        scrollSpeed: 800,
+        autoScrollDelay: 1500,
+        easing: 'linear'
+    });
+});
+
+
+
+
+// progress circle
+
+$('.circlechart').circlechart();
+
+
+//go to top
+
+$("#pageup").pageup();
+
+
+
+
